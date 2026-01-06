7a5dc36471dbe6013192991b6c034e3fc33e2510
Tomas Coox <tomas@joox.se>
2026-01-06 20:24:10 +0100

    remove cookie-banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
-import CookieConsent from '@/components/CookieConsent';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
 import "./globals.css";
@@ -63,7 +62,6 @@ export default function RootLayout({
         />
         {children}
         <Analytics />
-        <CookieConsent />
         <GoogleAnalytics gaId="G-3774YNSHKH" />
         <Script
           async
